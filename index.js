75b9fc5f70c866b05d7b4f4215447b78685828e9
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-09-15 22:45:36 -0400

    just the header is right

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-/***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const header = document.querySelector("#header");
+        console.log("Here's your header:", header);
+        header.style.color = "red";
+
+    })
 
 
-/***** Deliverable 2 *****/
 
 
-/***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+    /***** Deliverable 3 *****/
+
+    console.log("PLAYERS array looks like this:", PLAYERS)
+    debugger
+
+    function renderPlayers(PLAYERS) {
+        const playersLi = document.createElement('li')
+
+        for (const player in PLAYERS) {
+            playersLi.innerHTML = `
+                <div class="player" data-number= ${PLAYERS.number}>
+                <h3>
+                    ${PLAYERS.name} (<em>${PLAYERS.nickname}</em></h3>
+                        <img src="${PLAYERS.photo} 
+                        </div> `
+        }
+        playerDiv = document.querySelector('div.player-container')
+
+        // playerDiv.append(playersLi)
+    }
 
 
-/***** Deliverable 4 *****/
+
+
+
+    renderPlayers(PLAYERS)
+
+
+    /***** Deliverable 4 *****/
